1740ed177416268799ec42f8c7ffb21556454417
ZPSA <ZPSA@ZPSA_VIRTUAL>
2018-01-04 08:22:53 +0100

    no message

--- CSV_TO_SQL/Form1.Designer.cs
+++ CSV_TO_SQL/Form1.Designer.cs
@@ -44,7 +44,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(91, 136);
+            this.button2.Location = new System.Drawing.Point(177, 46);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(70, 39);
             this.button2.TabIndex = 1;
